a49fe8a3af5d61debbd4f7f732f3682285583a76
Kyle VanDeRiet <kylevdr@gmail.com>
2017-07-24 21:35:05 -0600

    Start building sessions/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
 
-  resources :users
-
-  post '/signup',  to: 'users#create'
-
-  get '/signup', to: 'users#new'
+  root 'static_pages#home'
 
   get '/home', to: 'static_pages#home'
-
   get '/help', to: 'static_pages#help'
-
   get '/about', to: 'static_pages#about'
-
   get '/contact', to: 'static_pages#contact'
 
-  root 'static_pages#home'
+  post '/signup',  to: 'users#create'
+  get '/signup', to: 'users#new'
+  resources :users
+
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
