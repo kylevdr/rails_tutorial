29101d693e37e6ce22d645ce0cfc4b08b8fec84d
Kyle VanDeRiet <kylevdr@gmail.com>
2017-08-06 14:39:05 -0600

    Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
   get '/home', to: 'static_pages#home'
@@ -12,6 +16,7 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   get '/login',   to: 'sessions#new'
   post '/login',   to: 'sessions#create'
