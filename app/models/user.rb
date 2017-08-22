43b652fa73ab37927d86b37e9914dc69a1f53e16
Kyle VanDeRiet <kylevdr@gmail.com>
2017-08-21 21:24:01 -0600

    Feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,9 @@ class User < ApplicationRecord
   # Defines a proto-feed.
   # See "Following users" for the full implementation.
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", following_ids: following_ids, user_id: id)
   end
 
   # Remembers a user in the database for use in persistent sessions.
